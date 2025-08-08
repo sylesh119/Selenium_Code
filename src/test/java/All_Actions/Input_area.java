ce280db8bdaee3e78737bad4a6ca8f5379fe30f9
sylesh <152365399+sylesh119@users.noreply.github.com>
2025-08-08 08:15:10 +0530

    Update Input_area.java
    test

--- src/test/java/All_Actions/Input_area.java
+++ src/test/java/All_Actions/Input_area.java
@@ -12,7 +12,7 @@ public class Input_area {
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://testautomationpractice.blogspot.com/");
 		driver.manage().window().maximize();
-		driver.findElement(By.xpath("//input[@id='name']")).sendKeys("Sylesh_badBoy");
+		driver.findElement(By.xpath("//input[@id='name']")).sendKeys("Sylesh_911");
 		driver.findElement(By.xpath("//input[@id='email']")).sendKeys("Sylesh@gmail.com");
 		driver.findElement(By.xpath("//input[@id='phone']")).sendKeys("1234567");
 		System.out.println("Test is Passed successfully...");
