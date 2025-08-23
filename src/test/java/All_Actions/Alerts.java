2045cf2d63995993ca04bf47cb58c0272f471e1f
sylesh <152365399+sylesh119@users.noreply.github.com>
2025-08-23 14:10:17 +0530

    new addition

---
+++ src/test/java/All_Actions/Alerts.java
@@ -0,0 +1,51 @@
+package All_Actions;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Alerts {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://testautomationpractice.blogspot.com/");
+		driver.manage().window().maximize();
+		//1) simple alert with ok button
+//		driver.findElement(By.id("alertBtn")).click();
+//		Thread.sleep(5000);
+//		Alert myalert = driver.switchTo().alert();
+//		System.out.println(myalert.getText());
+//		myalert.accept();
+//		driver.quit();
+		
+		// 2) confirmation alert with ok and cancel button 
+//		driver.findElement(By.id("confirmBtn")).click();
+//		Thread.sleep(5000);
+//		Alert myalert = driver.switchTo().alert();
+//		System.out.println("alert text: "+myalert.getText());
+//		myalert.accept();
+//		WebElement text = driver.findElement(By.xpath("//p[@id='demo']"));
+//		System.out.println(text.getText());
+//		Thread.sleep(2000);
+//		driver.quit();
+		
+		//3) prompt alert with message !!
+		
+		driver.findElement(By.id("promptBtn")).click();
+		Thread.sleep(3000);
+		Alert myalert = driver.switchTo().alert();
+		myalert.sendKeys("charan");
+		myalert.accept();
+		WebElement text = driver.findElement(By.xpath("//p[@id='demo']"));
+		System.out.println(text.getText());
+		Thread.sleep(2000);
+		driver.quit();
+		
+
+	}
+
+}
