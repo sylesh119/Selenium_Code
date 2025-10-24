a2eb20a9dc889ad9d667ba45672a383e22e32816
syles <syles@Sylesh>
2025-10-24 22:17:23 +0530

    changes made

---
+++ src/test/java/All_Actions/hello.java
@@ -0,0 +1,23 @@
+package All_Actions;
+
+import java.util.Scanner;
+
+public class hello {
+	
+	
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("enter first number:");
+		int a = sc.nextInt();
+		
+		System.out.print("enter secvdds number:");
+		int b = sc.nextInt();
+		
+		int c = a+b;
+		
+		System.out.println("The sum of "+ a + " and " + b + " is: " + c);
+		
+		sc.close();
+	}
+
+}
