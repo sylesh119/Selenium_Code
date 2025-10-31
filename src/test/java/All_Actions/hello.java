324edd4d505df6357f48f15d2ee77118b1aa1629
sylesh119 <syleshrockzz@gmail.com>
2025-10-31 09:51:59 +0530

    changes

--- src/test/java/All_Actions/hello.java
+++ src/test/java/All_Actions/hello.java
@@ -8,10 +8,17 @@ public class hello {
 	public static void main(String args[]) {
 		ArrayList<String> arr = new ArrayList<String>();
 
-		arr.add("sylesh");
+		arr.add("vijay");
 		arr.add(null);
 		arr.add(2, "daya");
+		arr.add("vijay");
+		// arr.removeFirst();
 		System.out.println(arr.size());
+		if (arr.contains("daya")) {
+			System.out.println("he is dumb");
+		} else {
+			System.out.println("he is good");
+		}
 	}
 
 }
